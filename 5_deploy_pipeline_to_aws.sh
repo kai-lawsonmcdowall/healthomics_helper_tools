3a4658d1818a3534e3032984221d9b939f6b36d8
Kai Lawson-McDowall <k.lawson-mcdowall@sonraianalytics.com>
2024-08-08 14:23:46 +0100

    auto determine path to parameter-template.json from healthomics folder

--- 5_deploy_pipeline_to_aws.sh
+++ 5_deploy_pipeline_to_aws.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Determine the parent directory of the script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PARENT_DIR=$(dirname "$SCRIPT_DIR")
+
+# Default parameter template file path in the parent directory
+DEFAULT_PARAM_TEMPLATE="$PARENT_DIR/parameter-template.json"
+read -p "Enter the name of the parameter template file (default: $DEFAULT_PARAM_TEMPLATE): " PARAMETER_TEMPLATE
+PARAMETER_TEMPLATE=${PARAMETER_TEMPLATE:-$DEFAULT_PARAM_TEMPLATE}
+
 # Prompt for ZIP file name
 read -p "Enter the name of the ZIP file (without extension): " ZIP_FILE_NAME
 
 # Prompt for pipeline name
 read -p "Enter the name of the pipeline: " PIPELINE_NAME
-
-# Default parameter template file path
-DEFAULT_PARAM_TEMPLATE="parameter_template.json"
-read -p "Enter the name of the parameter template file (default: $DEFAULT_PARAM_TEMPLATE): " PARAMETER_TEMPLATE
-PARAMETER_TEMPLATE=${PARAMETER_TEMPLATE:-$DEFAULT_PARAM_TEMPLATE}
 
 # Prompt for S3 path, defaulting to a specific path if not provided
 DEFAULT_S3_PATH="s3://jazzpharma-sonrai-pipelines/workflows/"
@@ -19,16 +23,12 @@ S3_PATH=${S3_PATH:-$DEFAULT_S3_PATH}
 # Ensure the S3 path ends with a slash
 [[ "${S3_PATH: -1}" != "/" ]] && S3_PATH="${S3_PATH}/"
 
-# Determine the parent directory of the current directory
-CURRENT_DIR=$(pwd)
-PARENT_DIR=$(dirname "$CURRENT_DIR")
-
 # Create a ZIP file of the parent directory, excluding healthomics_helper_tools and the .git
 ZIP_FILE="$ZIP_FILE_NAME.zip"
 echo "Creating ZIP file: $ZIP_FILE from the parent directory excluding healthomics_helper_tools"
 cd "$PARENT_DIR" || { echo "Failed to navigate to the parent directory"; exit 1; }
-zip -r "$ZIP_FILE" . -x "./$(basename "$CURRENT_DIR")/healthomics_helper_tools/*" || { echo "Failed to create ZIP file"; exit 1; }
-cd "$CURRENT_DIR" || { echo "Failed to return to the original directory"; exit 1; }
+zip -r "$ZIP_FILE" . -x "./$(basename "$SCRIPT_DIR")/healthomics_helper_tools/*" || { echo "Failed to create ZIP file"; exit 1; }
+cd "$SCRIPT_DIR" || { echo "Failed to return to the original directory"; exit 1; }
 
 # Upload the ZIP file to S3
 S3_FULL_PATH="${S3_PATH}${ZIP_FILE}"
@@ -40,7 +40,7 @@ echo "Creating Omics workflow: $PIPELINE_NAME"
 aws omics create-workflow \
     --name "$PIPELINE_NAME" \
     --definition-uri "$S3_FULL_PATH" \
-    --parameter-template "file://$PARENT_DIR/$PARAMETER_TEMPLATE" \
+    --parameter-template "file://$PARAMETER_TEMPLATE" \
     --engine NEXTFLOW || { echo "Failed to create Omics workflow"; exit 1; }
 
 echo "Workflow created successfully."
